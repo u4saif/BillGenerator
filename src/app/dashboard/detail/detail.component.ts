e5295bd6323efc8163224250c0bb6f76f8b83261
Shaikh Saifuddin <shaikh.saifuddin@xebia.com>
2021-05-26 01:12:43 +0530

    added logic for payment

--- src/app/dashboard/detail/detail.component.ts
+++ src/app/dashboard/detail/detail.component.ts
@@ -1,7 +1,8 @@
-import {   
+import {
   AngularFirestore,
   AngularFirestoreCollection,
-  AngularFirestoreDocument, } from '@angular/fire/firestore';
+  AngularFirestoreDocument,
+} from '@angular/fire/firestore';
 import { AppServiceService } from './../../services/app-service.service';
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
@@ -9,63 +10,82 @@ import { ActivatedRoute, Router } from '@angular/router';
 @Component({
   selector: 'app-detail',
   templateUrl: './detail.component.html',
-  styleUrls: ['./detail.component.css']
+  styleUrls: ['./detail.component.css'],
 })
 export class DetailComponent implements OnInit {
-  id:string;
-  userArr=[];
-  isGenBill:boolean = false;
-  cread;
-  pread=0;
-  date= new Date().toLocaleDateString();
-  unit=0;
-  amount=10;
-  billTableData:any =[];
+  id: string;
+  userArr = [];
+  isGenBill: boolean = false;
+  cread=0;
+  pread = 0;
+  date = new Date().toLocaleDateString();
+  unit = 0;
+  amount = 0;
+  payStaus:boolean=false;
+  billTableData: any = [];
 
-  constructor(private dataService: AppServiceService,private route:ActivatedRoute, private afs:AngularFirestore) { }
+  constructor(
+    private dataService: AppServiceService,
+    private route: ActivatedRoute,
+    private afs: AngularFirestore
+  ) {}
 
   ngOnInit(): void {
-      this.route.queryParams.subscribe((param)=>{
-        this.id=JSON.parse(atob(param.id));
-      });
-       this.dataService.getBills(this.id).subscribe((a)=> {
-       this.billTableData=[];
-       a.forEach((b)=>{
+    this.route.queryParams.subscribe((param) => {
+      this.id = JSON.parse(atob(param.id));
+    });
+    this.dataService.getBills(this.id).subscribe((a) => {
+      this.billTableData = [];
+      a.forEach((b) => {
         this.billTableData.push(b.data);
-       });
-       this.billTableData.sort((a,b) =>{
-        let c = new Date(a.date);
-        let d= new Date(b.date);
-        return c > d ? 1 : c < d ? -1 : 0;
-     
-    }).reverse();
-      
       });
-     this.dataService.getTanentList().subscribe((users)=>{
-      users.forEach((user)=>{
-        if(user.id==this.id) { this.userArr.push(user)};
-        
-      })
-     })
-    
+      this.billTableData
+        .sort((a, b) => {
+          let c = new Date(a.date);
+          let d = new Date(b.date);
+          return c > d ? 1 : c < d ? -1 : 0;
+        })
+        .reverse();
+        this.pread=this.billTableData[0]?.cread;
+    });
+    this.dataService.getTanentList().subscribe((users) => {
+      users.forEach((user) => {
+        if (user.id == this.id) {
+          this.userArr.push(user);
+        }
+      });
+    });
   }
 
-  GenBill(id){
-    this.isGenBill=true;
-    console.log("hi", this.billTableData);
- 
+  GenBill(id) {
+    this.isGenBill = true;
+    console.log('hi', this.billTableData);
   }
 
-  close(){
-    this.isGenBill=false;
+  close() {
+    this.isGenBill = false;
   }
-  done(){
-    this.afs.collection(`/tanent/${this.id}/bills`).add({'cread':this.cread,'pread':this.pread,'date':this.date,'unit':this.unit,'amount':this.amount});
+  done() {
+    this.afs
+      .collection(`/tanent/${this.id}/bills`)
+      .add({
+        cread: this.cread,
+        pread: this.pread,
+        date: this.date,
+        unit: this.unit,
+        amount: this.amount,
+        payStatus:this.payStaus
+      });
     alert('User Created Sucessfully!');
-    this.cread=null;
-    this.isGenBill=false;
-    
+    this.cread = null;
+    this.unit=0;
+    this.amount=0;
+    this.isGenBill = false;
   }
 
-
+  calAmt(val){
+    console.log(val);
+    this.unit= Number(this.cread)-Number(this.pread);
+    this.amount= this.unit*7;
+  }
 }
